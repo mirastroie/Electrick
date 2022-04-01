5664436017d2f630eb80b17cc2e208b4b61c16ba
Anghelache Vlad <anghelache.vlad.1@gmail.com>
2022-04-01 19:06:18 +0300

    Added styled map

--- app/src/main/java/com/example/electrick/MapsFragment.java
+++ app/src/main/java/com/example/electrick/MapsFragment.java
@@ -5,6 +5,8 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,6 +18,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -25,9 +28,11 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class MapsFragment extends Fragment {
     private FirebaseFirestore db;
+    private static final String TAG = MapsFragment.class.getSimpleName();
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
         /**
@@ -39,11 +44,27 @@ public class MapsFragment extends Fragment {
          * install it inside the SupportMapFragment. This method will only be triggered once the
          * user has installed Google Play services and returned to the app.
          */
+
+
         @Override
+
         public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+            try {
+
+                boolean success = googleMap.setMapStyle(
+                        MapStyleOptions.loadRawResourceStyle(getContext(),R.raw.map_style));
+
+                if (!success) {
+                    Log.e(TAG, "Style parsing failed.");
+                }
+            } catch (Resources.NotFoundException e) {
+                Log.e(TAG, "Can't find style. Error: ", e);
+            }
+
+
+            LatLng bucharest = new LatLng(44.43225, 26.10626);
+            googleMap.addMarker(new MarkerOptions().position(bucharest).title("Bucharest"));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bucharest,11));
         }
     };
 
